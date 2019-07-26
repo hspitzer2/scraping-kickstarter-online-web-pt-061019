2b66216f3f228027f4d029c180b8610d2514b7ad
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-26 22:24:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,10 @@
-require 'nokogiri'
+require "nokogiri"
+require "pry"
+
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
